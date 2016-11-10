36d019ac4e3b29b39ee8ee90ca8d183969810afe
SAC-CS112-Ton-Albert <albertton@Alberts-MacBook-Pro.local>
2016-11-09 18:44:52 -0800

    DiceGameObjects
    DiceGameObjects

--- src/dicegame/Dice.java
+++ src/dicegame/Dice.java
@@ -6,18 +6,33 @@
 package dicegame;
 
 import javax.swing.JOptionPane;
+import java.util.Random;
 
 public class Dice {
 	private int value;
 	private int predictionNumber;
         private int win;
         private int round;
+	private int times;
+        private int high;
+        private int HistoryNumThrows;
+        
+	public Dice() {
+            HistoryNumThrows = 0;
+            high = 6;
+        }
 	
-	public Dice() {}
-	
-	public void setRolls(int times)
+	public void setRolls(int number)
 	{
-		this.value = value;
+		HistoryNumThrows++;
+		high = high * number;
+		
+                Random rand = new Random();
+                int randomInt = rand.nextInt(3);
+                
+               
+                int value = ( 1 + (int) (Math.random() * high) );
+                
 	}
 		
 public void Correct(int predictionNumber)
@@ -35,10 +50,27 @@ public void Stats(int predictionNumber )
                 if(this.value == predictionNumber)
                     win++;
                 
-}
-
-}
+        }
+                                
+public void HistoryStats(int win)
+	{
+		JOptionPane.showMessageDialog (null,"Games won:" + win);
+	}
 	
+public void History()
+	{
+		JOptionPane.showMessageDialog (null, "Number of throws: " + HistoryNumThrows);
+                
+              
+	}
+public void WinPercentage()
+        {
+		JOptionPane.showMessageDialog (null, "Number of throws: " + win/round);
+                
+              
+	}
+	
+}
 
 	
 
